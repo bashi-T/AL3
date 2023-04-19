20c8f6cfbd5d0d5fbd0635f149e181e9db958a14
正己 續橋 <k022g0080@g.neec.ac.jp>
2023-04-19 14:25:28 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -61,6 +61,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	gameScene = new GameScene();
 	gameScene->Initialize();
 
+
+
+
+
+
+
+
+
+
 	// メインループ
 	while (true) {
 		// メッセージ処理
@@ -74,6 +83,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		input->Update();
 		// ゲームシーンの毎フレーム処理
 		gameScene->Update();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		// 軸表示の更新
 		axisIndicator->Update();
 		// ImGui受付終了
@@ -83,6 +107,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		dxCommon->PreDraw();
 		// ゲームシーンの描画
 		gameScene->Draw();
+
+
+
+
+
+
+
+
+
+
+
+
 		// 軸表示の描画
 		axisIndicator->Draw();
 		// プリミティブ描画のリセット
