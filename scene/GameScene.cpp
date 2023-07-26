db3e9ed79f48db5f997beb3dea9b0642d27096b3
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2023-07-26 15:25:18 +0900

    ittan

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -43,11 +43,10 @@ void GameScene::Initialize()
 void GameScene::Update()
 {
 	player_->Update();
-	player_->Rotate();
-	debugCamera_->Update();
 	enemy_->Update();
 	skydome_->Update();
 	CheckAllCollitions();
+	debugCamera_->Update();
 #ifdef _DEBUG
 	if (input_->TriggerKey(DIK_S))
 	{
